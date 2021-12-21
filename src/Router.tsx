6973b1f98cd1ecc3cafad07d0dfc93ceac6b649a
boudrioux brian <brian1.boudrioux@epitech.eu>
2021-12-21 16:14:19 +0100

    add private routes and logout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,21 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import { useSelector } from 'react-redux';
+import { userStore } from './types/user.types';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>} />
-            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard' element={<PrivateRoute component={<Dashboard />} />} />
         </Routes>
     )
+}
+
+const PrivateRoute = ({ component: Component }: { component: JSX.Element }) => {
+    const userState = useSelector((state: { user: userStore }) => state.user);
+    return !userState.isLogged ? <Navigate to="/" /> : Component;
 }
 
 
